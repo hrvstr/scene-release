0231acd7fb9a3b6532fbd810650a8a40604e70fa
HRVSTR <r.muhler@web.de>
2020-05-25 17:09:28 +0200

    add support for file extensions

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ module.exports = function(name){
     year: match(name, /(?!^)(?!1080|2160)[1,2]\d{3}/),
     resolution: match(name, /\d{3,4}p/i),
     type: match(name, /(WEB-)*DL|WEBDL|CAM|\.(T|W)S(?!C)|TELESYNC|(DVD|BD)SCR|SCR|DDC|R5[\.\s]LINE|R5|(DVD|U*(HD)|BR|BD|WEB)Rip|WEB|DVDR|(HD|PD)TV|BluRay/i),
-    source: match(name, /NETFLIX|AMZN|HULU|COOK/),
+    source: match(name, /NETFLIX|AMZN|HULU|COOK|PBS/),
     video: match(name, /NTSC|PAL|[xh][\.\s]?26(4|5)|XVID|HEVC/i),
     audio: match(name, /AAC2[\.\s]0|AAC|AC3|DTS|DD\S*(5|2)\.(1|0)/i),
     language: match(name, /(?!\.)(MULTiSUBS|MULTi|NORDiC|DANiSH|SWEDiSH|NORWEGiAN|GERMAN|iTALiAN|FRENCH|SPANiSH)(?=\.)/i),
     edition: match(name, /UNRATED|DC|(Directors|EXTENDED)[\.\s](CUT|EDITION)|EXTENDED|3D|2D|\bNF\b/i),
     tags: name.match(/COMPLETE|LiMiTED|iNTERNAL/i),
     release: match(name, /REAL[\.\s]PROPER|PROPER|REPACK|READNFO|READ[\.\s]NFO|DiRFiX|NFOFiX/i),
-    group: match(name, /[A-Za-z0-9]+$/)
+    group: match(name, /(?<=\-)[A-Za-z0-9]+/),
+    extension: match (name, /(?<=\.)\w+$/)
   };
 
   var matches = '';
