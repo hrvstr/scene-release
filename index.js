0c47d8ba6db4c6fe362811c1981cf08b22ac01b4
HRVSTR <r.muhler@web.de>
2020-05-25 17:24:58 +0200

    removed extension support again :D

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = function(name){
     date: match(name, /\d{4}(\.\d{2}){2}/),
     year: match(name, /(?!^)(?!1080|2160)[1,2]\d{3}/),
     resolution: match(name, /\d{3,4}p/i),
-    type: match(name, /(WEB-)*DL|WEBDL|CAM|\.(T|W)S(?!C)|TELESYNC|(DVD|BD)SCR|SCR|DDC|R5[\.\s]LINE|R5|(DVD|U*(HD)|BR|BD|WEB)Rip|WEB|DVDR|(HD|PD)TV|BluRay/i),
+    type: match(name, /CAM|TS(?!C)|TELESYNC|(DVD|BD)SCR|SCR|DDC|R5[\.\s]LINE|R5|(DVD|HD|BR|BD|WEB)Rip|DVDR|(HD|PD)TV|WEB-DL|WEBDL|BluRay/i),
     source: match(name, /NETFLIX|AMZN|HULU|COOK|PBS/),
     video: match(name, /NTSC|PAL|[xh][\.\s]?26(4|5)|XVID|HEVC/i),
     audio: match(name, /AAC2[\.\s]0|AAC|AC3|DTS|DD\S*(5|2)\.(1|0)/i),
@@ -17,8 +17,7 @@ module.exports = function(name){
     edition: match(name, /UNRATED|DC|(Directors|EXTENDED)[\.\s](CUT|EDITION)|EXTENDED|3D|2D|\bNF\b/i),
     tags: name.match(/COMPLETE|LiMiTED|iNTERNAL/i),
     release: match(name, /REAL[\.\s]PROPER|PROPER|REPACK|READNFO|READ[\.\s]NFO|DiRFiX|NFOFiX/i),
-    group: match(name, /(?<=\-)[A-Za-z0-9]+/),
-    extension: match (name, /(?<=\.)\w+$/)
+    group: match(name, /[A-Za-z0-9]+$/)
   };
 
   var matches = '';
