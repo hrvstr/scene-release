6258a23e79357ce357de31311d4f38a347b10263
Richard Muhler <hrvstr@MacBook-Pro.fritz.box>
2020-05-30 00:43:46 +0200

    fix tags

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ module.exports = function(name){
     audio: match(name, /(?<=\.)(DUBBED|AAC2[\.\s]0|AAC|AC3|DTS(-HD)?|(DD|MA)\S?(5|2)\.(1|0))/i),
     language: match(name, /(?<=\.)(MULTiSUBS|MULTi|NORDiC|DANiSH|SWEDiSH|NORWEGiAN|GERMAN|iTALiAN|FRENCH|SPANiSH)(?=\.)/i),
     edition: match(name, /(?<=\.)(UNRATED|DC|(Directors|EXTENDED)[\.\s](CUT|EDITION)|EXTENDED|REMASTERED|3D|2D|\bNF\b)(?=\.)/i),
-    tags: name.match(/COMPLETE|LiMiTED|iNTERNAL|DOKU|DOCU/i),
+    tags: match(name, /COMPLETE|LiMiTED|iNTERNAL|DOKU|DOCU/i),
     release: match(name, /REAL[\.\s]PROPER|PROPER|REPACK|READNFO|READ[\.\s]NFO|DiRFiX|NFOFiX/i),
     group: match(name, /[A-Za-z0-9]+$/)
   };
